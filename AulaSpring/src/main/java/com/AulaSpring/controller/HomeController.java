626ea4cae5bf81d17f4c420afa26a7bc8780be3a
Aluno <Aluno@LAB0614>
2020-02-28 21:42:03 -0300

    inicio aula web

---
+++ AulaSpring/src/main/java/com/AulaSpring/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.AulaSpring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+@RequestMapping(method = RequestMethod.GET,value = "/")
+public ModelAndView inicio(){
+	ModelAndView mv = new ModelAndView("produto/Cadproduto");
+	return mv;
+}
+}
