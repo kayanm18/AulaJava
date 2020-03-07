abd8d9129d4fbd6e9b1398b6e0d0da96d7946c4d
Aluno <Aluno@LAB0615>
2020-03-06 22:03:52 -0300

    Aula2 conexão com bancos e criação de tabelas automaticas.

---
+++ AulaSpring/src/main/java/com/AulaSpring/demo/modelos/Pessoa.java
@@ -0,0 +1,58 @@
+package com.AulaSpring.demo.modelos;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pessoa {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String nome;
+	private int idade;
+	private String cpf;
+	private String rg;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+}
---
+++ AulaSpring/src/main/java/com/AulaSpring/demo/modelos/Produto.java
@@ -0,0 +1,60 @@
+package com.AulaSpring.demo.modelos;
+
+import java.math.BigInteger;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String nome;
+	private int unidade;
+	private BigInteger valor;
+	private String descrição;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getUnidade() {
+		return unidade;
+	}
+
+	public void setUnidade(int unidade) {
+		this.unidade = unidade;
+	}
+
+	public BigInteger getValor() {
+		return valor;
+	}
+
+	public void setValor(BigInteger valor) {
+		this.valor = valor;
+	}
+
+	public String getDescrição() {
+		return descrição;
+	}
+
+	public void setDescrição(String descrição) {
+		this.descrição = descrição;
+	}
+
+}
